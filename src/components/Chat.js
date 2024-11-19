5e1954cb896d35a982afe8aa5424a5853b74592f
codewithbirat1 <biratpandey9@gmail.com>
2024-11-19 21:46:25 +0545

    responsive

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,10 @@
-
 import ChatWindow from './ChatWindow';
 import UserPanel from './UserPanel';
+
 const Chat = () => {
   return (
-    <div className="flex h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
-      <aside className="w-1/4 bg-gray-200  dark:bg-gray-800 text-gray-800 dark:text-gray-100 flex flex-col">
+    <div className="flex flex-col md:flex-row h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+      <aside className="w-full md:w-1/4 bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-100 flex flex-col">
         <UserPanel />
       </aside>
       <main className="flex-1 bg-gray-100 dark:bg-gray-900">
--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,7 +1,6 @@
 const ChatWindow = () => {
   return (
-    <div className="p-4 bg-gray-100 dark:bg-gray-800 transition-colors duration-300 h-full">
-     
+    <div className="p-4 bg-gray-100 dark:bg-gray-800 h-full flex flex-col">
     </div>
   );
 };
