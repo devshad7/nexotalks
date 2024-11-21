d78c0fbe05e468ea9df9e6b796b715f9d075e9e8
Md Shad Khan <devshaad@Mac.lan>
2024-11-21 09:33:07 +0530

    feat: soome changes

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -1,15 +1,13 @@
+import Chat from '@/components/Chat';
 import Sidebar from '@/components/Sidebar';
-import Chat from '@/components/Chat'
 import React from 'react';
 
 export default function Page() {
 
   return (
     <>
-    
       <Sidebar />
       <Chat />
-      
     </>
   );
 }
